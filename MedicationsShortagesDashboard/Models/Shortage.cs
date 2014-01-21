5e4b0949ab25a56b418c56b0836f22b80d02d5a5
Cameron Keif <cameron.keif@gmail.com>
2014-01-21 13:18:45 -0500

    Created Shortage class and accompanying DbContext.

---
+++ MedicationsShortagesDashboard/Models/Shortage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+
+namespace MedicationsShortagesDashboard.Models
+{
+    /* Good means no shortage
+     * Warning means below normal supply
+     * Severe means there is no supply available
+     */
+
+    public enum StatusCondition { GOOD, WARNING, SEVERE }
+    public class Shortage
+    {
+        public string drugName { get; set; }
+        public StatusCondition status { get; set; }
+        public string sourceURL { get; set; }
+    }
+
+    public class ShortageDbContext : DbContext
+    {
+        public DbSet<Shortage> Shortages { get; set; }
+    }
+}

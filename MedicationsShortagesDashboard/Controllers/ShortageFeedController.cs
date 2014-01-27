fd835badd2cbec16a097d0a15200b433df3c2bd9
Cameron Keif <cameron.keif@gmail.com>
2014-01-26 19:27:20 -0500

    ShortageFeedParser dumps out contents of feed. #babysteps

---
+++ MedicationsShortagesDashboard/Controllers/ShortageFeedController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+
+namespace MedicationsShortagesDashboard.Controllers
+{
+    public class ShortageFeedController : Controller
+    {
+        //
+        // GET: /ShortageFeed/
+
+        public string Index()
+        {
+            XmlReader reader = XmlReader.Create("http://www.ashp.org/rss/shortages/#current");
+            List<string> things = new List<String>();
+
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        //writer.WriteStartElement(reader.Name);
+                        break;
+                    case XmlNodeType.Text:
+                        //writer.WriteString(reader.Value);
+                        things.Add(reader.Value);
+                        break;
+                    case XmlNodeType.XmlDeclaration:
+                    case XmlNodeType.ProcessingInstruction:
+                        //writer.WriteProcessingInstruction(reader.Name, reader.Value);
+                        break;
+                    case XmlNodeType.Comment:
+                        //writer.WriteComment(reader.Value);
+                        break;
+                    case XmlNodeType.EndElement:
+                        //writer.WriteFullEndElement();
+                        break;
+                }
+            }
+            String returnStr = "<u>stuff</u>";
+
+            foreach(String thing in things) {
+                returnStr = returnStr + "<br>" + thing;
+            }
+            return returnStr;
+        }
+
+    }
+}

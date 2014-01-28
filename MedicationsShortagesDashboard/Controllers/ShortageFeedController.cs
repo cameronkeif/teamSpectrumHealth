dc8959c09dd05ec1645defcab7701c4901e8e358
Cameron Keif <cameron.keif@gmail.com>
2014-01-28 15:36:33 -0500

    Stylecop

--- MedicationsShortagesDashboard/Controllers/ShortageFeedController.cs
+++ MedicationsShortagesDashboard/Controllers/ShortageFeedController.cs
@@ -1,33 +1,45 @@
-﻿using MedicationsShortagesDashboard.Models;
-using MedicationsShortagesDashboard.Utilities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Xml;
-
+﻿//-----------------------------------------------------------------------
+// <copyright file="ShortageFeedController.cs" company="Spectrum Health">
+//      Spectrum Health
+// </copyright>
+//-----------------------------------------------------------------------
 namespace MedicationsShortagesDashboard.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using System.Xml;
+    using MedicationsShortagesDashboard.Models;
+    using MedicationsShortagesDashboard.Utilities;
+
+    /// <summary>
+    /// Temporary controller used to display the results of the XML parsing.
+    /// </summary>
     public class ShortageFeedController : Controller
     {
-        //
-        // GET: /ShortageFeed/
-
+        /// <summary>
+        ///     Default page
+        /// </summary>
+        /// <returns>HTML string</returns>
         public string Index()
         {
-            String returnStr = "<u>WARNING</u><br><table border=\"1\">";
-            List<Shortage> shortages = rssFeedParser.parseAshpFeed("http://www.ashp.org/rss/shortages/#current", StatusCondition.WARNING); // Parse the shortage feed
-            foreach(Shortage shortage in shortages) {
+            string returnStr = "<u>WARNING</u><br><table border=\"1\">";
+            List<Shortage> shortages = RssFeedParser.ParseAshpFeed("http://www.ashp.org/rss/shortages/#current", StatusCondition.WARNING); // Parse the shortage feed
+            foreach (Shortage shortage in shortages) 
+            {
                 returnStr += "<tr><td>" + shortage.drugName + "</td><td>" + shortage.status.ToString() + "</td><td>" + shortage.sourceURL + "</td></tr>";
             }
 
             returnStr += "</table><br><br><u>SEVERE</u><br><table border=\"1\">";
 
-            shortages = rssFeedParser.parseAshpFeed("http://www.ashp.org/rss/notavailable/#notavailable", StatusCondition.SEVERE); // Parse the shortage feed
-            foreach(Shortage shortage in shortages) {
+            shortages = RssFeedParser.ParseAshpFeed("http://www.ashp.org/rss/notavailable/#notavailable", StatusCondition.SEVERE); // Parse the shortage feed
+            foreach (Shortage shortage in shortages) 
+            {
                 returnStr += "<tr><td>" + shortage.drugName + "</td><td>" + shortage.status.ToString() + "</td><td>" + shortage.sourceURL + "</td></tr>";
             }
+
             returnStr += "</table>";
             return returnStr;
         }

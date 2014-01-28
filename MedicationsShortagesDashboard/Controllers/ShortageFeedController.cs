e23083fed26b47d5bc8b8cd626856ad935449ea7
Cameron Keif <cameron.keif@gmail.com>
2014-01-27 21:26:21 -0500

    Pull XML parsing stuff from Controller to a separate rssParser class.

--- MedicationsShortagesDashboard/Controllers/ShortageFeedController.cs
+++ MedicationsShortagesDashboard/Controllers/ShortageFeedController.cs
@@ -1,4 +1,5 @@
 ﻿using MedicationsShortagesDashboard.Models;
+using MedicationsShortagesDashboard.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,53 +17,19 @@ namespace MedicationsShortagesDashboard.Controllers
         public string Index()
         {
             String returnStr = "<u>WARNING</u><br><table border=\"1\">";
-            List<Shortage> shortages = parseAshpFeed("http://www.ashp.org/rss/shortages/#current", StatusCondition.WARNING); // Parse the shortage feed
+            List<Shortage> shortages = rssFeedParser.parseAshpFeed("http://www.ashp.org/rss/shortages/#current", StatusCondition.WARNING); // Parse the shortage feed
             foreach(Shortage shortage in shortages) {
                 returnStr += "<tr><td>" + shortage.drugName + "</td><td>" + shortage.status.ToString() + "</td><td>" + shortage.sourceURL + "</td></tr>";
             }
 
             returnStr += "</table><br><br><u>SEVERE</u><br><table border=\"1\">";
 
-            shortages = parseAshpFeed("http://www.ashp.org/rss/notavailable/#notavailable", StatusCondition.SEVERE); // Parse the shortage feed
+            shortages = rssFeedParser.parseAshpFeed("http://www.ashp.org/rss/notavailable/#notavailable", StatusCondition.SEVERE); // Parse the shortage feed
             foreach(Shortage shortage in shortages) {
                 returnStr += "<tr><td>" + shortage.drugName + "</td><td>" + shortage.status.ToString() + "</td><td>" + shortage.sourceURL + "</td></tr>";
             }
             returnStr += "</table>";
             return returnStr;
-        }
-
-        private List<Shortage> parseAshpFeed(string URL, StatusCondition status)
-        {
-            XmlReader reader = XmlReader.Create(URL);
-            List<Shortage> shortages = new List<Shortage>();
-            String tag = String.Empty, link = String.Empty, drugName = String.Empty;
-
-            //TODO clean up this. I think it can be made better -CK
-            while (reader.Read())
-            {
-                switch (reader.NodeType)
-                {
-                    case XmlNodeType.Element:
-                        tag = reader.Name;
-                        break;
-                    case XmlNodeType.Text:
-                        if (tag.Equals("category"))
-                        {
-                            drugName = reader.Value.Replace("Shortage Bulletin", String.Empty);
-                        }
-                        if (tag.Equals("link"))
-                        {
-                            // The top of the feed also contains links. Need to ensure these links are actually for drug shortage information
-                            if (!String.IsNullOrEmpty(drugName))
-                            {
-                                shortages.Add(new Shortage(drugName, status, reader.Value));
-                            }
-                        }
-                        break;
-                }
-            }
-
-            return shortages;
         }
     }
 }
---
+++ MedicationsShortagesDashboard/Utilities/rssFeedParser.cs
@@ -0,0 +1,46 @@
+﻿using MedicationsShortagesDashboard.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml;
+
+namespace MedicationsShortagesDashboard.Utilities
+{
+    public class rssFeedParser
+    {
+        public static List<Shortage> parseAshpFeed(string URL, StatusCondition status)
+        {
+            XmlReader reader = XmlReader.Create(URL);
+            List<Shortage> shortages = new List<Shortage>();
+            String tag = String.Empty, link = String.Empty, drugName = String.Empty;
+
+            //TODO clean up this. I think it can be made better -CK
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        tag = reader.Name;
+                        break;
+                    case XmlNodeType.Text:
+                        if (tag.Equals("category"))
+                        {
+                            drugName = reader.Value.Replace("Shortage Bulletin", String.Empty);
+                        }
+                        if (tag.Equals("link"))
+                        {
+                            // The top of the feed also contains links. Need to ensure these links are actually for drug shortage information
+                            if (!String.IsNullOrEmpty(drugName))
+                            {
+                                shortages.Add(new Shortage(drugName, status, reader.Value));
+                            }
+                        }
+                        break;
+                }
+            }
+
+            return shortages;
+        }
+    }
+}

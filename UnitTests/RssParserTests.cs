f1330c2a988ea55fe8a22fb24099190e13841a22
Cameron Keif <cameron.keif@gmail.com>
2014-02-01 20:15:40 -0500

    Add unit testing project to solution.

---
+++ UnitTests/RssParserTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MedicationsShortagesDashboard.Utilities;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class RssParserTests
+    {
+        [TestMethod]
+        public void ASHP_WithValidFeed()
+        {
+            
+        }
+    }
+}

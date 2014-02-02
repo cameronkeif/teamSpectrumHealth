fcd683ba6630f8b9644e3c425ff2c1fdb3aff310
Cameron Keif <cameron.keif@gmail.com>
2014-02-01 22:16:27 -0500

    Add unit tests for PendingShortages.Equals

---
+++ UnitTests/PendingShortageTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using MedicationsShortagesDashboard.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class PendingShortageTests
+    {
+        [TestMethod]
+        public void Equals_Valid()
+        {
+            PendingShortage a = new PendingShortage("Ciprofloxacin Injection", "link1");
+            PendingShortage b = new PendingShortage("Ciprofloxacin Injection", "link1");
+
+            Assert.IsTrue(a.Equals(b));
+        }
+
+        [TestMethod]
+        public void Equals_BothFalse()
+        {
+            PendingShortage a = new PendingShortage("Ciprofloxacin Injection", "link1");
+            PendingShortage b = new PendingShortage("Hydromorphone Hydrochloride Injection", "link2");
+
+            Assert.IsFalse(a.Equals(b));
+        }
+
+        [TestMethod]
+        public void Equals_DrugNameFalse()
+        {
+            PendingShortage a = new PendingShortage("Ciprofloxacin Injection", "link1");
+            PendingShortage b = new PendingShortage("Hydromorphone Hydrochloride Injection", "link1");
+
+            Assert.IsFalse(a.Equals(b));
+        }
+
+        [TestMethod]
+        public void Equals_SourceUrlFalse()
+        {
+            PendingShortage a = new PendingShortage("Ciprofloxacin Injection", "link1");
+            PendingShortage b = new PendingShortage("Ciprofloxacin Injection", "link2");
+
+            Assert.IsFalse(a.Equals(b));
+        }
+    }
+}

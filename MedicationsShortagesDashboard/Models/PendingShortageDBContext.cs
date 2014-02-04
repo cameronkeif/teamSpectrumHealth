9de7f7e34d24da552a11e6ea1128b2902aaf5219
Cameron Keif <cameron.keif@gmail.com>
2014-02-03 22:08:47 -0500

    Create DB Context for pending shortages

---
+++ MedicationsShortagesDashboard/Models/PendingShortageDBContext.cs
@@ -0,0 +1,41 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="PendingShortageDBContext.cs" company="Spectrum Health">
+//      Spectrum Health
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace MedicationsShortagesDashboard.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Web;
+
+    /// <summary>
+    /// DB Context for Pending Shortages
+    /// </summary>
+    public class PendingShortageDBContext : DbContext
+    {
+        /// <summary>
+        /// Collection of all entities in the DB context
+        /// </summary>
+        private DbSet<PendingShortage> pendingShortages;
+
+        /// <summary>
+        /// Gets or sets pendingShortages
+        /// </summary>
+        public DbSet<PendingShortage> PendingShortages
+        {
+            get
+            {
+                return this.pendingShortages;
+            }
+
+            set
+            {
+                this.pendingShortages = value;
+            }
+        }
+    }
+}

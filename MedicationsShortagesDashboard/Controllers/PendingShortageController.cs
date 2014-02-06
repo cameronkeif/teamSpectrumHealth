b028095495bd5f603b39aca067f15823976581cf
Cam Keif <cameron.keif@gmail.com>
2014-02-05 22:31:56 -0500

    RESTful query to read all entries in PENDING_SHORTAGES

---
+++ MedicationsShortagesDashboard/Controllers/PendingShortageController.cs
@@ -0,0 +1,41 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="PendingShortageController.cs" company="Spectrum Health">
+//      Spectrum Health
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace MedicationsShortagesDashboard.Controllers
+{
+    using System.Web.Http;
+    using MedicationsShortagesDashboard.Models;
+    using MedicationsShortagesDashboard.Services;
+
+    /// <summary>
+    /// Provides a Restful interaction between the application and the
+    /// PENDING_SHORTAGES table in the database.
+    /// </summary>
+    public class PendingShortageController : ApiController
+    {
+        /// <summary>
+        /// Performs the actual querying of the database.
+        /// </summary>
+        private PendingShortageRepository pendingShortageRepository;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PendingShortageController"/> class
+        /// </summary>
+        public PendingShortageController()
+        {
+            this.pendingShortageRepository = new PendingShortageRepository();
+        }
+
+        /// <summary>
+        /// HTTP Get
+        /// </summary>
+        /// <returns>All of the entries in the PENDING_SHORTAGES table.</returns>
+        public PendingShortage[] Get()
+        {
+            return this.pendingShortageRepository.GetAllContacts();
+        }
+    }
+}
---
+++ MedicationsShortagesDashboard/Services/PendingShortageRepository.cs
@@ -0,0 +1,35 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="PendingShortageRepository.cs" company="Spectrum Health">
+//      Spectrum Health
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace MedicationsShortagesDashboard.Services
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using MedicationsShortagesDashboard.Models;
+
+    /// <summary>
+    /// Used to select entries in the PENDING_SHORTAGES table
+    /// </summary>
+    public class PendingShortageRepository
+    {
+        /// <summary>
+        /// Gets every entry in PENDING_SHORTAGES
+        /// </summary>
+        /// <returns>An array containing all of the entries in PENDING_SHORTAGES</returns>
+        public PendingShortage[] GetAllContacts()
+        {
+            PendingShortageDBContext db = new PendingShortageDBContext();
+            List<PendingShortage> pendingShortages = new List<PendingShortage>();
+
+            foreach (PendingShortage pendingShortage in db.PendingShortages.ToList())
+            {
+                pendingShortages.Add(pendingShortage);
+            }
+
+            return pendingShortages.ToArray();
+        }
+    }
+}

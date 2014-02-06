dea716d5388a88bc03308d842d1d74519c9678c9
Cam Keif <cameron.keif@gmail.com>
2014-02-06 00:56:20 -0500

    Create shitty view that displays all pending shortages

---
+++ MedicationsShortagesDashboard/Controllers/PendingShortagesListController.cs
@@ -0,0 +1,26 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="PendingShortagesListController.cs" company="Spectrum Health">
+//      Spectrum Health
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace MedicationsShortagesDashboard.Controllers
+{
+    using System.Web.Mvc;
+
+    /// <summary>
+    /// Controller for displaying all pending shortages
+    /// </summary>
+    public class PendingShortagesListController : Controller
+    {
+        /// <summary>
+        /// Get index page
+        /// </summary>
+        /// <returns>Index page view</returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}

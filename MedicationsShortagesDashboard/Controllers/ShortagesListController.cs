b6a3a80f29ec84878d1ccc7eacff56760bc326fc
Cameron Keif <cameron.keif@gmail.com>
2014-02-09 19:46:43 -0500

    Create shitty view to output all of the shortages in the DB (this likely will later be removed, unless this functionality is desired obvs)

---
+++ MedicationsShortagesDashboard/Controllers/ShortagesListController.cs
@@ -0,0 +1,26 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ShortagesListController.cs" company="Spectrum Health">
+//      Spectrum Health
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace MedicationsShortagesDashboard.Controllers
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System.Web.Mvc;
+
+    /// <summary>
+    /// Controller for displaying all pending shortages
+    /// </summary>
+    public class ShortagesListController : Controller
+    {
+        /// <summary>
+        /// Get index page
+        /// </summary>
+        /// <returns>Index page view</returns>
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}

667ce6d39c772ec3d23d2e52a45012a1013b448b
Cameron Keif <cameron.keif@gmail.com>
2014-02-17 21:10:28 -0500

    Begin Unit tests for shortages
    This is just a quick little test, I'm going to wrap these up after our alpha, doing feature work until then.

---
+++ UnitTests/ShortageControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MedicationsShortagesDashboard.Models;
+using Moq;
+using MedicationsShortagesDashboard.Controllers;
+using MedicationsShortagesDashboard.Services;
+using System.Collections.Generic;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ShortageControllerTests
+    {
+        [TestMethod]
+        public void ShortageControllerGetTest()
+        {
+            Shortage[] expectedResults = new Shortage[]
+            {
+                new Shortage(0, 1, DateTime.Now, "warning"),
+                new Shortage(1, 2, DateTime.Now, "warning")
+            };
+
+            var mock = new Mock<ShortageRepository>();
+            mock.Setup(x => x.GetAllShortages()).Returns(expectedResults);
+
+            ShortageController controller = new ShortageController(mock.Object);
+
+            List<Shortage> result = new List<Shortage>(controller.Get());
+            foreach (Shortage shortage in expectedResults)
+            {
+                Assert.IsTrue(result.Contains(shortage));
+            }
+        }
+    }
+}

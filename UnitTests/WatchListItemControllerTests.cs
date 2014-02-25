a60ec668c1c4451c32eb790e9797e2b8d864b0c6
Cameron Keif <cameron.keif@gmail.com>
2014-02-25 14:08:13 -0500

    WatchListItemController unit tests

---
+++ UnitTests/WatchListItemControllerTests.cs
@@ -0,0 +1,29 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="WatchListItemControllerTests.cs" company="Spectrum Health">
+//      Spectrum Health
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace UnitTests
+{
+    using MedicationsShortagesDashboard.Controllers;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Unit tests for WatchListItemController
+    /// </summary>
+    [TestClass]
+    public class WatchListItemControllerTests
+    {
+        /// <summary>
+        /// Tests default constructor
+        /// </summary>
+        [TestMethod]
+        public void WatchListItemControllerDefaultConstructor()
+        {
+            WatchListItemController controller = new WatchListItemController();
+
+            Assert.IsNotNull(controller);
+        }
+    }
+}

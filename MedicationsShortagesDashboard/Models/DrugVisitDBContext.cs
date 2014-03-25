4b8b79eae52846e365259197dbabffc68be43752
Alex Lockwood <beholdtheleviathan@gmail.com>
2014-03-25 19:12:29 -0400

    Some missing files #stomachpunches

---
+++ MedicationsShortagesDashboard/Models/DrugVisitDBContext.cs
@@ -0,0 +1,42 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="DrugVisitDBContext.cs" company="Spectrum Health">
+//      Spectrum Health
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace MedicationsShortagesDashboard.Models
+{
+    using System.Data.Entity;
+
+    /// <summary>
+    /// DB Context for drug visits
+    /// </summary>
+    public class DrugVisitDBContext : DbContext
+    {
+        /// <summary>
+        /// Collection of all entities in the DB context
+        /// </summary>
+        private DbSet<DrugVisit> drugVisits;
+
+        public DrugVisitDBContext()
+        {
+            Database.SetInitializer<Models.DrugVisitDBContext>(null);
+        }
+
+        /// <summary>
+        /// Gets or sets drug visits
+        /// </summary>
+        public DbSet<DrugVisit> DrugVisits
+        {
+            get
+            {
+                return this.drugVisits;
+            }
+
+            set
+            {
+                this.drugVisits = value;
+            }
+        }
+    }
+}
---
+++ MedicationsShortagesDashboard/Services/DrugVisitRepository.cs
@@ -0,0 +1,84 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="DrugVisitRepository.cs" company="Spectrum Health">
+//      Spectrum Health
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace MedicationsShortagesDashboard.Services
+{
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Linq;
+    using MedicationsShortagesDashboard.Models;
+
+    public class DrugVisitRepository
+    {
+        /// <summary>
+        /// Database context
+        /// </summary>
+        private DrugVisitDBContext db;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DrugVisitRepository" /> class
+        /// </summary>
+        public DrugVisitRepository()
+        {
+            this.db = new DrugVisitDBContext();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DrugVisitRepository" /> class
+        /// </summary>
+        /// <param name="context">A database context</param>
+        public DrugVisitRepository(DrugVisitDBContext context)
+        {
+            this.db = context;
+        }
+
+        /// <summary>
+        /// Gets every entry in DRUG_VISIT
+        /// </summary>
+        /// <returns>An array containing all of the entries in DRUG_VISIT</returns>
+        public virtual DrugVisit[] GetAllDrugVisits()
+        {
+            List<DrugVisit> drugVisits = new List<DrugVisit>();
+
+            foreach (DrugVisit drugVisit in this.db.DrugVisits.ToList())
+            {
+                drugVisits.Add(drugVisit);
+            }
+
+            return drugVisits.ToArray();
+        }
+
+        /// <summary>
+        /// Gets the drug visit from the database with matching ID
+        /// </summary>
+        /// <param name="id">The ID of the drug visit we're looking for.</param>
+        /// <returns>The drug visit which has id as it's ID.</returns>
+        public DrugVisit GetDrugVisit(int id)
+        {
+            return this.db.DrugVisits.Find(id);
+        }
+
+        /// <summary>
+        /// Add a message to the database
+        /// </summary>
+        /// <param name="message">The message to add to the database.</param>
+        public void AddDrugVisit(DrugVisit drugVisit)
+        {
+            this.db.DrugVisits.Add(drugVisit);
+            this.db.SaveChanges();
+        }
+
+        /// <summary>
+        /// Updates an existing drug visit
+        /// </summary>
+        /// <param name="drugVisit">The drug visit to modify.</param>
+        public void UpdateDrug(DrugVisit drugVisit)
+        {
+            this.db.Entry(drugVisit).State = EntityState.Modified;
+            this.db.SaveChanges();
+        }
+    }
+}
